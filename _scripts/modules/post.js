52e1840353cb4fa2239d21978eefa8fb4e488b35
Simon Walsh <simon@walsh.si>
2016-12-17 18:32:34 -0500

    Building a nabber and anchor links

--- _scripts/modules/post.js
+++ _scripts/modules/post.js
@@ -11,7 +11,36 @@ const Post = {
   // Globals
   //
   $article: $('.s-post .m-article'),
-  init () {}
+
+  buildNav () {
+    let $headings = this.$article.find('.m-article-content h2')
+    let $nav = this.$article.find('.m-article-nav-list')
+    if ($headings.length < 1) return
+
+    $headings.each(function () {
+      let $heading = $(this)
+      $heading.wrap(`<a href="#${$heading.attr('id')}"></a>`)
+    })
+
+    if ($headings.length < 3) return
+
+    this.$article.addClass('has-nav')
+    $headings.each(function () {
+      let $heading = $(this)
+      let template = `
+        <li>
+          <a href="#${$heading.attr('id')}">
+            ${$heading.text()}
+          </a>
+        </li>
+      `
+      $nav.append(template)
+    })
+  },
+
+  init () {
+    this.buildNav()
+  }
 }
 
 //
